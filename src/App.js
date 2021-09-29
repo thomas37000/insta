82a38ecbe546ad81daa1c2001aac73f4b77beceb
Thomas <thomas@avecnous.eu>
2021-09-29 11:54:20 +0200

    create a fake api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import React from 'react';
 import logo from './logo.svg';
 import Home from './components/Home';
+import Grid from './components/Grid';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
       <Home />
+      <Grid />
     </div>
   );
 }
---
+++ src/components/Grid.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Grid.css';
+
+const Grid = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="box"></div>
+      </div>
+    </div>
+  );
+};
+
+Grid.propTypes = {};
+
+export default Grid;
