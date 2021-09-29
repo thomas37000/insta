07f38c0f81345784cbc19c5e4b2f6ca5bc49bd9e
Thomas <thomas@avecnous.eu>
2021-09-29 15:04:05 +0200

    add jpg to src img

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 const Card = ({ img, name }) => {
   return (
     <div className="Card">
-      <img src="https://i.imgflip.com/5olrz8.jpg" alt={name} />
       <div className="CardImg">
         <img src={img} alt={name} />
       </div>
