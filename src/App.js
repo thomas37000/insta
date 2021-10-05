d4ca13707378673b3ca192941d9af93720c2f5cb
Thomas <thomas@avecnous.eu>
2021-10-05 18:00:15 +0200

    install reactstrap carousel

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Home from './components/Home';
 import Grid from './components/Grid';
 import Nav from './components/Nav';
 import './App.css';
+import Slider from './components/Slider';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Slider />
       <Home />
       <Grid />
     </div>
---
+++ src/components/Slider.jsx
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
+} from 'reactstrap';
+
+const Slider = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      // 'https://raw.githubusercontent.com/thomas37000/insta/master/fake-api.json'
+      'https://api.thedogapi.com/v1/breeds'
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log('useEffect', data);
+        // setItems(data.instagram);
+        setItems(data);
+      });
+  }, []);
+
+  // const fetchDogs =
+  //   items.length > 0 &&
+  //   items.map((dog, i) => {
+  //     return <CardDogs key={i} {...dog} />;
+  //   });
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
+
+  const slides = items.map((item) => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.img}
+      >
+        {/* <img src={item.img} alt={item.name} /> */}
+        <img src={item.image.url} alt={item.name} className="cards-image" />
+        <CarouselCaption captionText={item.name} captionHeader={item.name} />
+      </CarouselItem>
+    );
+  });
+
+  return (
+    <Carousel
+      activeIndex={activeIndex}
+      next={next}
+      previous={previous}
+      interval={false}
+    >
+      <CarouselIndicators
+        items={items}
+        activeIndex={activeIndex}
+        onClickHandler={goToIndex}
+      />
+      {slides}
+      <CarouselControl
+        direction="prev"
+        directionText="Previous"
+        onClickHandler={previous}
+      />
+      <CarouselControl
+        direction="next"
+        directionText="Next"
+        onClickHandler={next}
+      />
+    </Carousel>
+  );
+};
+
+export default Slider;
