f248c96a67231f17a9cb74385d4f0ab9876a62ed
Thomas <thomas@avecnous.eu>
2021-10-18 18:13:12 +0200

    continue css responsive

--- src/components/Profil/Profil.jsx
+++ src/components/Profil/Profil.jsx
@@ -36,69 +36,70 @@ function Profil(id) {
     ovnis.map((ovni) => {
       return (
         <>
-        
-            <div className="divProfil">
-              <img src={ovni.image} className="profil" alt={ovni.name} />
-            </div>
-            <div className="profil-description">
-              <div className="profil-header">
-                <div className="profil-name">
-                  <h2>{ovni.name}</h2>
-                  <div className="btn-profil">
-                    <button
-                      type="submit"
-                      className="follow-me"
-                      // onClick={()=>()}
-                    >
-                      Follow
-                    </button>
-                    <button
-                      type="button"
-                      className="select"
-                      // onClick={()=>()}
-                    >
-                      <FontAwesomeIcon
-                        icon={faChevronDown}
-                        className="chevron"
-                      />
-                    </button>
-                  </div>
+          <div className="divProfil">
+            <img src={ovni.image} className="profil" alt={ovni.name} />
+          </div>
+          <div className="profil-description">
+            <div className="profil-header">
+              <div className="profil-name">
+                <h2>{ovni.name}</h2>
+                <div className="btn-profil">
+                  <button
+                    type="submit"
+                    className="follow-me"
+                    // onClick={()=>()}
+                  >
+                    Follow
+                  </button>
+                  <button
+                    type="button"
+                    className="select"
+                    // onClick={()=>()}
+                  >
+                    <FontAwesomeIcon icon={faChevronDown} className="chevron" />
+                  </button>
+                </div>
+                <button
+                  type="button"
+                  className="select btn-ellipsis"
+                  // onClick={()=>()}
+                >
                   <FontAwesomeIcon icon={faEllipsisH} className="ellipsis" />
+                </button>
+              </div>
+            </div>
+            <div className="desc-container">
+              <div className="follow">
+                <div className="">
+                  <span className="span-profil">23</span> posts
+                </div>
+                <div className="">
+                  <span className="span-profil">12O</span> followers
+                </div>
+                <div className="">
+                  <span className="span-profil">221</span> following
                 </div>
               </div>
-              <div className="desc-container">
-                <div className="follow">
-                  <div className="">
-                    <span className="span-profil">23</span> posts
-                  </div>
-                  <div className="">
-                    <span className="span-profil">12O</span> followers
-                  </div>
-                  <div className="">
-                    <span className="span-profil">221</span> following
-                  </div>
-                </div>
 
-                <div className="profil-desc">
-                  <div>
-                    <h1 className="desc-name">{ovni.name}</h1>
-                  </div>
-                  <div className="">Developper Web Junior from Nantes</div>
-                  <div className="">Javascript / React</div>
-                  <div className="github">
-                    <a
-                      className="github-link"
-                      href="https://github.com/thomas37000"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      https://github.com/thomas37000
-                    </a>
-                  </div>
+              <div className="profil-desc">
+                <div>
+                  <h1 className="desc-name">{ovni.name}</h1>
+                </div>
+                <div className="">Developper Web Junior from Nantes</div>
+                <div className="">Javascript / React</div>
+                <div className="github">
+                  <a
+                    className="github-link"
+                    href="https://github.com/thomas37000"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    https://github.com/thomas37000
+                  </a>
                 </div>
               </div>
             </div>
-        
+          </div>
         </>
       );
     });
